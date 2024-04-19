ad5858977a3dadb40b330207e0ee81780d6a3792
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-04-19 11:01:56 +0545

    Time: 32 ms (85.53%) | Memory: 16.4 MB (99.44%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,28 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = [0]*3
-        for num in nums:
-            if num==0:
-                count[0]+=1
-            elif num==1:
-                count[1]+=1
+
+        low = mid = 0
+        high = len(nums)-1
+        
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                mid+=1
+                low+=1
+            elif nums[mid]==2:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high-=1
             else:
-                count[2]+=1
-        j=0
-        i=0
-        while j<3:
-            if count[j]>0:
-                if j==0:
-                    nums[i]=0
-                elif j==1:
-                    nums[i]=1
-                else:
-                    nums[i]=2
-                i+=1
-                count[j]-=1
-            else:
-                j+=1
-            
+                mid+=1
+        
+
 
                 
