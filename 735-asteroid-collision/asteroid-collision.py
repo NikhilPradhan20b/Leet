bde8def3988e89f73ff22edf454b826b6b33229c
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-04-21 15:04:19 +0545

    Time: 87 ms (31.58%) | Memory: 17.8 MB (90.24%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -3,7 +3,7 @@ class Solution:
         stack = []
         for i in range(len(asteroids)):
             stack.append(asteroids[i])
-            while len(stack)>1 and math.copysign(1,stack[-2])==1 and math.copysign(1,stack[-1])==-1:
+            while len(stack)>1 and stack[-2]>0 and stack[-1]<0:
                 if abs(stack[-1])>abs(stack[-2]):
                     stack.pop()
                     stack.pop()
