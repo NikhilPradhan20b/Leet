915167309b2b0b9585fd76134c5c0271232eb2de
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-04-24 17:12:19 +0545

    Time: 42 ms (17.66%) | Memory: 16.6 MB (66.15%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        count = 0
+        inside = False
+        for index,value in enumerate(s):
+            if not inside:
+                if value=='*':
+                    count+=1
+                elif value =='|':
+                    if inside:
+                        inside = False
+                    else:
+                        inside = True
+            else:
+                if value =='|':
+                    if inside:
+                        inside = False
+                    else:
+                        inside = True
+            
+        return count
+            
+            
+        
