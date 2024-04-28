88cff40257d6bf52af15a089fcc3a8e1f1e9c263
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-04-28 11:26:24 +0545

    Time: 38 ms (30.54%) | Memory: 16.6 MB (61.11%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        i=0
+        hasm={}
+        s  = s.split()
+        while i<len(s) or i<len(pattern):
+            if i>=len(pattern):
+                return False
+            if i>=len(s):
+                return False
+            if pattern[i] not in hasm.keys():
+                if s[i] not in set(hasm.values()):
+                    hasm[pattern[i]] = s[i]
+                else:
+                    return False
+            else:
+                if hasm[pattern[i]]!=s[i]:
+                    return False
+                # elif s[i] in set(hasm.values()):
+                #     return False
+            i+=1
+            print(i)
+        return True
+        
