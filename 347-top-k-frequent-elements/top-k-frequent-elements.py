befa4ad8ea700e3d20fc618b187fc37b45da995d
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-01 23:26:37 +0545

    Time: 88 ms (63.11%) | Memory: 21.3 MB (41.27%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hasm2 = defaultdict(list)
+        hasm = {}
+        ans = []
+        for each in nums:
+            if each not in hasm.keys():
+                hasm[each] = 1
+            else:
+                hasm[each]+=1
+        print(hasm)
+        
+        for key, value in hasm.items():
+                hasm2[value].append(key)
+        print(hasm2)
+            
+        new = list(hasm2.keys())
+        new.sort()
+        new.reverse()
+        i=0
+        while k>0:
+            for each in hasm2[new[i]]:
+                ans.append(each)
+                k-=1
+            i+=1
+        return ans
