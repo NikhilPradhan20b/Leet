7d5a206827c3898c2cbbb16b0821e79292764068
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-04 09:58:47 +0545

    Time: 342 ms (83.94%) | Memory: 23.3 MB (93.50%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -4,18 +4,12 @@ class Solution:
         total = 0
         j = len(people)-1
         i=0
-        print(people)
         while i<=j:
             diff = limit - people[j]
             total+=1
-            # while diff>0:
             if people[i]<=diff:
                 diff -= people[i]
                 i+=1
-            # elif i==j:
-            #     diff = 0
-            # else:
-            #     diff = 0
             j-=1  
         return total
 
