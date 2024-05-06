251b00ab735553ce50ba5bbd8c59685f16bbe7a5
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-06 15:27:32 +0545

    Time: 619 ms (5.02%) | Memory: 60.3 MB (32.15%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_node = head
+        arr = []
+        stack= []
+        while new_node is not None:
+            # print(new_node.val)
+            # if new_node.next is not None:
+            #     if new_node.val < new_node.next.val:
+            #         arr.append(new_node.next.val)
+            arr.append(new_node.val)
+            new_node = new_node.next
+        for i in range(len(arr)):
+            if stack:
+                if stack[-1]>arr[i]:
+                    stack.append(arr[i])
+                else:
+                    while stack:
+                        if stack[-1]<arr[i]:
+                            stack.pop()
+                        else:
+                            break
+                    stack.append(arr[i])
+            else:
+                stack.append(arr[i])
+        print(stack)
+        
+        if not stack:
+            return head
+        else:
+            new_head = ptr = ListNode()
+            for i in range(len(stack)):
+                ptr.val = stack[i]
+                if i<len(stack)-1:
+                    ptr.next = ListNode()
+                    ptr = ptr.next
+            
+            return new_head
+        
