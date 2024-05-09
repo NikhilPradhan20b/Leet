e26d95e31216158fbf8705e94df6e9d228d4fa01
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-09 10:39:42 +0545

    Time: 38 ms (34.14%) | Memory: 16.7 MB (9.71%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -2,8 +2,7 @@ class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         if ch in word:
             point = word.index(ch)
-            ans = word[:point+1:][::-1]
-            ans+= word[point+1:]
+            ans = word[:point+1:][::-1] + word[point+1:]
             return ans
         else:
             return word
