4ab713b42195fdf0fc51edc648fd833082c0645d
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-10 08:52:09 +0545

    Time: 1319 ms (33.90%) | Memory: 112.2 MB (12.54%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,8 +1,6 @@
 from collections import defaultdict
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        ans = []
-        small = 1
         i = 0
         j = 1
         hasm=defaultdict(list)
@@ -11,17 +9,8 @@ class Solution:
                 i+=1
                 j=i+1
             else:
-                # print(arr[i]/arr[j])
-                # if arr[i]/arr[j] < small:
-                #     small = arr[i]/arr[j]
-                #     if ans:
-                #         ans.pop()
-                #         ans.pop()
-                #     ans.append(arr[i])
-                #     ans.append(arr[j])
                 hasm[arr[i]/arr[j]] = [arr[i],arr[j]]
                 j+=1
         x = sorted(list(hasm.keys()))
-        # print(x[k-1])
         return hasm[x[k-1]]
 
