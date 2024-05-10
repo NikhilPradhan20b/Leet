989a54ae6d316ac72b582a2f486879b04012c35c
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-10 16:01:03 +0545

    Time: 64 ms (48.88%) | Memory: 18.1 MB (6.39%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hasm={}
+        for i in range(len(nums)):
+            hasm[target-nums[i]] = i
+
+        print(hasm)
+        for j in range(len(nums)):
+            if nums[j] in hasm.keys() and hasm[nums[j]]!=j:
+                return [hasm[nums[j]],j] 
+        
