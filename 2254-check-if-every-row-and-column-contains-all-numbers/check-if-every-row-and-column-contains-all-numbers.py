1cde4cc94cfdd209c2c80150dc5ac67ebdb70903
NikhilPradhan20b <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-11 19:14:08 +0545

    Time: 581 ms (54.84%) | Memory: 17 MB (91.58%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
@@ -3,18 +3,19 @@ class Solution:
         check = set()
         for i in range(1,len(matrix)+1):
             check.add(i)
-        print(check)
         for i in range(len(matrix)):
             row_check = set()
             column_check = set()
             for j in range(len(matrix)):
                 row_check.add(matrix[j][i])
                 column_check.add(matrix[i][j])
-            if len(row_check)!=len(check) or len(column_check)!=len(check):
+            # print(row_check,column_check,check,row_check==check)
+            # if len(row_check)!=len(check) or len(column_check)!=len(check):
+            #     return False
+            # else:
+            #     for each in check:
+            #         if each not in row_check or each not in column_check:
+            #             return False
+            if row_check!=check or column_check!=check:
                 return False
-            else:
-                for each in check:
-                    if each not in row_check or each not in column_check:
-                        print(each)
-                        return False
         return True
