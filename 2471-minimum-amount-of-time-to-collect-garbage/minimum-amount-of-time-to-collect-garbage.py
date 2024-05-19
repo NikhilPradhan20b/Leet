a989b5165dcad4013e98e631f40e9d5a63894269
Nikhil Pradhan <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-05-19 14:11:02 +0545

    Time: 769 ms (62.52%) | Memory: 38.7 MB (36.90%) - LeetSync

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -0,0 +1,34 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        def find_sum(garbage,total=0):
+            for i in range(len(garbage)):
+                temp = garbage[i].count('G')
+                if temp>0:
+                    total += temp
+                temp = garbage[i].count('P')
+                if temp>0:
+                    total += temp
+                temp = garbage[i].count('M')
+                if temp>0:
+                    total += temp
+            return total
+            
+        def find(garbae,s):
+            for j in range(len(garbage)-1,0,-1):
+                if s in garbage[j]:
+                    return j
+            return 0
+
+        def cal_sum(travel,distance):
+            total = 0
+            for i in range(distance):
+                total+=travel[i]
+            return total
+            
+            
+        g_len,p_len,m_len =find(garbage,'G'),find(garbage,'P'),find(garbage,'M')
+
+        total = find_sum(garbage)+cal_sum(travel,g_len)+cal_sum(travel,p_len)+cal_sum(travel,m_len)
+
+
+        return (total)
