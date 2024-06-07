da5e1c4dd55c4870cd952642d2192a52aaabe1e3
Nikhil Pradhan <86955208+NikhilPradhan20b@users.noreply.github.com>
2024-06-07 08:55:27 +0545

    Time: 228 ms (24.72%) | Memory: 24.9 MB (94.66%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,13 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        arr = sentence.split()
+        for i in range(len(arr)):
+            l=float('inf')
+            for each in dictionary: 
+                n = len(each)
+                word = arr[i][:n]
+                if each==word:
+                    if n<l:
+                        arr[i]=each
+                        l=n
+        return " ".join(arr)
